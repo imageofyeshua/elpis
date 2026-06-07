48922e54b4e4bd2da2cadddd257cdc9abdd61296
Daniel Park <intheimageofyeshua@gmail.com>
2026-06-07 16:46:05 +0900

    DB Migration to VERSION=0 with Command rails db:migrate VERSION=0

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_07_002037) do
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "email"
-    t.string "name"
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[8.1].define(version: 0) do
 end
