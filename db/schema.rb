5276c72a8f30c9d0cc0998316068fc1c5c2227e5
Daniel Park <intheimageofyeshua@gmail.com>
2026-06-07 09:23:38 +0900

    Create Micropost Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_06_135223) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_07_002037) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
