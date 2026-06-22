5dca6a4ee1007c67f99b5cf74cd7cb48e576d535
Daniel Park <intheimageofyeshua@gmail.com>
2026-06-22 23:55:31 +0900

    Finish Stimulus to Display Menu

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = [ "button" ]
+  static targets = [ "button", "menu" ]
 
   toggle(event) {
     event.preventDefault()
 
     this.buttonTarget.classList.toggle("open")
+    this.menuTarget.classList.toggle("flex")
+    this.menuTarget.classList.toggle("hidden")
   }
 }
