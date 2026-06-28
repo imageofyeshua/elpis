3cc6f48d542fcca84becfa9d5015c99f54b4776a
Daniel Park <intheimageofyeshua@gmail.com>
2026-06-28 23:16:58 +0900

    Create Rails Favorite Ruby Branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :users
 
-  get "home", to: "static_pages#home"
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
   get "loopstudios", to: "static_pages#loopstudios"
   get "shortly", to: "static_pages#shortly"
 
-  root "static_pages#shortly"
+  root "static_pages#home"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
