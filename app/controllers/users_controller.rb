db51151d42715c8e6bdf88dcd1b5ed9cb3eaa8f0
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-06 01:42:26 +0900

    Create User Params Method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
     else 
       render 'new', status: :unprocessable_entity
     end
   end
+
+  private
+
+    def user_params
+      params.expect(user: [:name, :email, :password, :password_confirmation])
+    end
 end
