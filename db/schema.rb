2a728d6d6151aadcd1a885c679586dd76e8724aa
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-07 06:33:48 +0900

    User Remember Digest Method with User.new_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_26_071529) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_06_212442) do
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "name", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
