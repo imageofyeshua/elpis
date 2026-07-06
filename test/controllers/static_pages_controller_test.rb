cbfedfc4bd90ded37e61e4f4f1fe9aae9194baac
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-06 22:19:43 +0900

    Valid User Login Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,22 +8,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Elpis"
   end
 
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | Elpis"
-  end
-
   test "should get about" do
     get about_path
     assert_response :success
     assert_select "title", "About | Elpis"
-  end
-
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | Elpis"
   end
 
 end
