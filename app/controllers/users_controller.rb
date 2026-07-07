2264f33214a0b7969039b1602a00b9c39771eda8
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-08 07:38:20 +0900

    Before Action for Non-Logged-In Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -38,5 +39,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.expect(user: [:name, :email, :password, :password_confirmation])
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:alert] = "Please log in."
+      redirect_to login_url, status: :see_other
+    end
   end
 end
