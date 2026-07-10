e22b5b464fe6b9b3901039c5033f3831e5933386
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-10 22:52:31 +0900

    User Index Test Incomplete

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "index including pagination" do
+    log_in_as(@user) 
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.grow'
+  end
+
 end
