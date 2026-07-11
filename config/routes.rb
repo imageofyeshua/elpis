e90179f685c3bfc7cfc7234092f4a9607199cbb4
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-11 21:14:42 +0900

    Add Migration : rails g migration add_activation_to_users

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
+  resources :account_activations, only: [:edit]
 
   # Tailwind CSS Practice Projects
   get "loopstudios", to: "static_pages#loopstudios"
