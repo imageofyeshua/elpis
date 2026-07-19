f33d8e017dd560ce5fd8a69b99f3d14cb22bc11d
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-19 22:44:13 +0900

    Profile Micropost Test

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include ApplicationHelper
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'span', text: @user.name
+    assert_match @user.microposts.count.to_s, response.body
+  end
 end
