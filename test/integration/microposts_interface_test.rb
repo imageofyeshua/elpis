9aa86e17b0b9ccf1840d050caa4b8618971bcfb6
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-22 22:25:05 +0900

    Fixed @feed_items&.any? method

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
-class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class MicropostsInterface < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+end
+
+class MicropostsInterfaceTest < MicropostsInterface
+
+  test "should paginate microposts" do
+    get root_path
+    assert_select 'div#pagination'
+  end
+
+  test "should show errors but not create micropost on invalid submission" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+  end
 end
