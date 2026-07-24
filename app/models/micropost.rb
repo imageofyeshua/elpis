78b32f864510aafa01ff6849455fa7234f095084
Daniel Park <intheimageofyeshua@gmail.com>
2026-07-25 08:34:19 +0900

    Add Micropost File Upload Size and Type Validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,20 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :image_type_and_size
+
+  private
+
+  def image_type_and_size
+    return unless image.attached?
+
+    if image.blob.byte_size > 1.megabytes
+      errors.add(:image, "should be less than 1MB")
+    end
+
+    acceptable_types = ["image/jpeg", "image/gif", "image/png", "image/webp"]
+    unless acceptable_types.include?(image.blob.content_type)
+      errors.add(:image, "must be a valid image format (JPEG, PNG, GIF, or WEBP)")
+    end
+  end
 end
